389e656dc9b2a935094c685598173c1fb5d06c29
Daria Malenko <malienko.99@ukr.net>
2021-10-15 19:17:49 +0300

    #3 MVC: Added more CMS test pages

--- src/Dariam/Cms/Controller/Page.php
+++ src/Dariam/Cms/Controller/Page.php
@@ -5,9 +5,20 @@ namespace Dariam\Cms\Controller;
 
 class Page implements \Dariam\Framework\Http\ControllerInterface
 {
+    private \Dariam\Framework\Http\Request $request;
+
+    /**
+     * @param \Dariam\Framework\Http\Request $request
+     */
+    public function __construct(
+        \Dariam\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     public function execute(): string
     {
-        $page = 'home.php';
+        $page = $this->request->getParameter('page') . '.php';
 
         ob_start();
         require_once "../src/page.php";
--- src/Dariam/Cms/Router.php
+++ src/Dariam/Cms/Router.php
@@ -7,12 +7,31 @@ use Dariam\Cms\Controller\Page;
 
 class Router implements \Dariam\Framework\Http\RouterInterface
 {
+    private \Dariam\Framework\Http\Request $request;
+
+    /**
+     * @param \Dariam\Framework\Http\Request $request
+     */
+    public function __construct(
+        \Dariam\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     /**
      * @inheritDoc
      */
     public function match(string $requestUrl): string
     {
-        if ($requestUrl === '') {
+        $cmsPage = [
+            '',
+            'test-page',
+            'test-page-2'
+        ];
+
+        if (in_array($requestUrl, $cmsPage)) {
+            $this->request->setParameter('page', $requestUrl ?: 'home');
+
             return Page::class;
         }
 

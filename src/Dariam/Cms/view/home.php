bc7ebd931a1b18e1f5901fd02c7295796546f6f7
Daria Malenko <malienko.99@ukr.net>
2021-12-22 17:30:56 +0200

    Lesson 10 - Create campus slider module, added styles, updated home page

--- src/Dariam/Cms/view/home.php
+++ src/Dariam/Cms/view/home.php
@@ -41,40 +41,46 @@
 </section>
 <section title="Recently Viewed Posts">
     <h2>Recently Viewed Posts</h2>
-    <div class="recently-viewed-slider-wrapper">
-        <div class="recently-viewed-slider">
-            <div class="post">
-                <a class="post-item-image" href="/post-1" title="Post 1">
-                    <img src="/images/post-placeholder.png" alt="Post 1" width="200"/>
-                </a>
-                <a class="post-item-title" href="/post-1" title="Post 1">Post 1</a>
-                <span>Robert Downey Jr.</span>
-                <p>2020.12.02</p>
-            </div>
-            <div class="post">
-                <a class="post-item-image" href="/post-2" title="Post 2">
-                    <img src="/images/post-placeholder.png" alt="Post 2" width="200"/>
-                </a>
-                <a class="post-item-title" href="/post-2" title="Post 2">Post 2</a>
-                <span>Chris Evans</span>
-                <p>2020.01.08</p>
-            </div>
-            <div class="post">
-                <a class="post-item-image" href="/post-3" title="Post 3">
-                    <img src="/images/post-placeholder.png" alt="Post 3" width="200"/>
-                </a>
-                <a class="post-item-title" href="/post-3" title="Post 3">Post 3</a>
-                <span>Scarlett Johansson</span>
-                <p>2019.02.14</p>
-            </div>
+    <div class="recently-viewed-slider-wrapper campus-slider">
+        <div class="post">
+            <a class="post-item-image" href="/post-1" title="Post 1">
+                <img src="/images/post-placeholder.png" alt="Post 1" width="200"/>
+            </a>
+            <a class="post-item-title" href="/post-1" title="Post 1">Post 1</a>
+            <span>Robert Downey Jr.</span>
+            <p>2020.12.02</p>
         </div>
-        <button class="slider-control-prev slider-control button-hover" type="button">
-            <span class="slider-control-prev-icon"><i class="fas fa-chevron-left"></i></span>
-            <span class="slider-control-prev-title">Previous</span>
-        </button>
-        <button class="slider-control-next slider-control button-hover" type="button">
-            <span class="slider-control-next-icon"><i class="fas fa-chevron-right"></i></span>
-            <span class="slider-control-prev-title">Next</span>
-        </button>
+        <div class="post">
+            <a class="post-item-image" href="/post-2" title="Post 2">
+                <img src="/images/post-placeholder.png" alt="Post 2" width="200"/>
+            </a>
+            <a class="post-item-title" href="/post-2" title="Post 2">Post 2</a>
+            <span>Chris Evans</span>
+            <p>2020.01.08</p>
+        </div>
+        <div class="post">
+            <a class="post-item-image" href="/post-3" title="Post 3">
+                <img src="/images/post-placeholder.png" alt="Post 3" width="200"/>
+            </a>
+            <a class="post-item-title" href="/post-3" title="Post 3">Post 3</a>
+            <span>Scarlett Johansson</span>
+            <p>2019.02.15</p>
+        </div>
+        <div class="post">
+            <a class="post-item-image" href="/post-4" title="Post 4">
+                <img src="/images/post-placeholder.png" alt="Post 4" width="200"/>
+            </a>
+            <a class="post-item-title" href="/post-4" title="Post 4">Post 4</a>
+            <span>Chris Hemsworth</span>
+            <p>2019.02.14</p>
+        </div>
+        <div class="post">
+            <a class="post-item-image" href="/post-5" title="Post 5">
+                <img src="/images/post-placeholder.png" alt="Post 5" width="200"/>
+            </a>
+            <a class="post-item-title" href="/post-5" title="Post 5">Post 5</a>
+            <span>Mark Ruffalo</span>
+            <p>2019.01.08</p>
+        </div>
     </div>
 </section>
